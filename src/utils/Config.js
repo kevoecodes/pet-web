8986d160d84520be0223df983b63f9f95f573c0c
kevoecodes <kelvinremmy01@gmail.com>
2023-05-10 14:23:28 +0300

    First Commit

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -3,7 +3,7 @@ export const Config = {
     // socketURL: 'ws://127.0.0.1:5500/',
     // mediaURL: 'http://localhost:5000/media/v1/'
     // mediaURL: 'http://161.35.210.153:5000/media/v1/',
-    apiURL: 'http://161.35.210.153:8080/'
+    apiURL: 'http://161.35.210.153:5500/'
     // apiURL: 'https://www.foleniqms.com/',
     // socketURL: "wss://www.foleniqms.com/",
     // mediaURL: 'https://www.foleniqms.com/streamer/v1/',
